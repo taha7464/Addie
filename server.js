086022cda3999cffe9809040c8c21af117468303
Taha Tariq <taha7464@gmail.com>
2017-05-02 19:04:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -17,21 +17,3 @@ var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-
-//
-// var http = require('http');
-// var express = require('express');
-// var twilio = require('twilio');
-//
-// var app = express();
-//
-// app.post('/sms', function(req, res) {
-// //  var twiml = new twilio.TwimlResponse();
-//   twiml.message('The Robots are coming! Head for the hills!');
-//   res.writeHead(200, {'Content-Type': 'text/xml'});
-//   res.end(twiml.toString());
-// });
-//
-// http.createServer(app).listen(1337, function () {
-//   console.log("Express server listening on port 1337");
-// });
